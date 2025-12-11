dbdc2d9716ec9625c82b977e0dc6efcfea6648e6
navetacandra <dev@navetacandraa.my.id>
2025-12-11 11:43:24 +0700

    create in_building

--- MainChar.gd
+++ MainChar.gd
@@ -1,42 +1,47 @@
 extends CharacterBody2D
 
-const SPEED = 85
-const ROTATION_SPEED = 2
-
 @onready var _animated_sprite = $CharSprite
 @export var use_sword = false
+@export var speed = 85
+@export var rotation_speed = 2
 
 var rotation_direction
 var move_axis
 var zombie_around = false
 var zombie = TYPE_NIL
 var last_hit: float
+var _delay = false
+var _debug_coordinate = false
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("ui_shift"):
 		var _time = Time.get_ticks_usec() / 1000**2
-		#print(_time - last_hit)
 		if _time - last_hit >= 0.5:
 			last_hit = _time
 			_animated_sprite.play("punch_p" if use_sword else "punch_tk")
-			if zombie_around:
-				print(zombie.hited)
 			if zombie_around:
 				if zombie.hited < 3:
 					zombie.hited += 1
 	else:
 		rotation_direction = Input.get_axis("ui_left", "ui_right")
 		move_axis = Input.get_axis("ui_up", "ui_down")
-		rotation += rotation_direction * ROTATION_SPEED * delta
+		rotation += rotation_direction * rotation_speed * delta
 		
 		if (move_axis && move_axis <= 0) || rotation_direction:
 			_animated_sprite.play("walk_p" if use_sword else "walk_tk")
 		else:
 			_animated_sprite.play("meneng_p" if use_sword else "meneng_tk")
+			if Input.is_action_pressed("ui_down") && !_delay:
+				_delay = true
+				rotation_degrees += 180
+				await get_tree().create_timer(0.25).timeout
+				_delay = false
 		
 		if move_axis <= 0:
-			velocity = transform.y * move_axis * SPEED * -1
+			velocity = transform.y * move_axis * speed * -1
 
+	if _debug_coordinate:
+		print(position)
 	move_and_slide()
 
 func _zombie_enter_area(body):
---
+++ in_building.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$MainChar.rotation_degrees = 180
+	$MainChar.speed = 50
+	$MainChar.rotation_speed = 1.5
+	$MainChar._debug_coordinate = true
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
